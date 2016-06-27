009e6babc85e4b11a105b916639782a0250fd22a
ssube <ssube@redbrickhealth.com>
2016-06-27 15:05:44 -0500

    Revert "clean up deploy branching"
    This reverts commit 0b7243c6a0a3bc42f4ec9e0b6cd71dba452afd7b.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,23 +2,20 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const gulpScreeps = require('gulp-screeps');
 const insert = require('gulp-insert');
+const credentials = require('./credentials.json');
 const eslint = require('gulp-eslint');
 
 const paths = {
     src: 'src/**/*.js'
 };
 
-const commitTask = (credentialFile) => {
-    return gulp.src('src/main.js')
+gulp.task('default', ['lint', 'commit']);
+
+gulp.task('commit', () =>
+    gulp.src('src/main.js')
         .pipe(webpack({ output: { filename: "main.js" } }))
-        .pipe(insert.prepend("module.exports = "))
-        .pipe(require(credentialFile)));
-};
-
-gulp.task('default', ['lint', 'commit-test']);
-
-gulp.task('commit-test', () => commitTask('./credentials-test.json'));
-gulp.task('commit-prod', () => commitTask('./credentials.json'));
+        .pipe(insert.prepend("module.exports = ")) // This line makes webpack's boilerplate be compatible with Screeps.
+        .pipe(gulpScreeps(credentials)));
 
 gulp.task('lint', () =>
     gulp.src(paths.src)
